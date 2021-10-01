690a8a4af449cf6d5e9d7a192a460444c3de4826
Michael <chimzeboi07@gmail.com>
2021-10-01 16:48:33 +0100

    updated contact-form

--- assests/js/index.js
+++ assests/js/index.js
@@ -2,14 +2,32 @@ const toggle = document.getElementById('toggle');
 const sidebar = document.getElementById('navlinks');
 
 
-document.onclick = function(e){
-    if(e.target.id !=='sidebar' && e.target.id !== 'toggle'){
-        toggle.classList.remove('active');
-        sidebar.classList.remove('active');
-    }
-}
+// document.onclick = function(e){
+//     if(e.target.id !=='sidebar' && e.target.id !== 'toggle'){
+//         toggle.classList.remove('active');
+//         sidebar.classList.remove('active');
+//     }
+// }
 
 toggle.addEventListener('click', () => {
     toggle.classList.toggle('active');
     sidebar.classList.toggle('active');
 });
+
+// dropdown
+function dropdown() {
+    document.getElementById('myDropdown').classList.toggle('show');
+}
+
+window.onclick = function(event){
+    if(!event.target.matches('.dropbtn')){
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for(i=0; i<dropdowns.length; i++){
+            let openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
